2dfac08b50e124b8a59271e71f846755cbfac255
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-11 20:13:16 +0000

    added input

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
-def greet():
-  print("Hello")
-  print("How are you?")
+def greet(name):
+  print(f"Hello {name}.")
+  print(f"How are you {name}?")
   print("Such a beautiful day, right?")
 
-greet()
+greet("Debora")
